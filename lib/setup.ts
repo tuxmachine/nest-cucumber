b2cb4587decbc84cf0ef999694abed5ef28e6729
Rick Dutour Geerling <info@rickdg.nl>
2022-12-19 16:09:20 +0100

    refactor: simplify world
    We expose fewer details and we don't need the hook, because the world is
    already created for each scenario.

--- lib/setup.ts
+++ lib/setup.ts
@@ -1,4 +1,4 @@
-import { Before, setWorldConstructor } from '@cucumber/cucumber';
+import { setWorldConstructor } from '@cucumber/cucumber';
 import { Type } from '@nestjs/common';
 import 'reflect-metadata';
 import { startApp } from './app';
@@ -6,10 +6,5 @@ import { NestWorld } from './world';
 
 export const bootstrap = (rootModule: Type) => {
   startApp(rootModule);
-
   setWorldConstructor(NestWorld);
-
-  Before(async function (this: NestWorld) {
-    await this.registerScenario();
-  });
 };
--- lib/world.ts
+++ lib/world.ts
@@ -1,11 +1,20 @@
-import { World } from '@cucumber/cucumber';
+import { IWorldOptions, World } from '@cucumber/cucumber';
 import { ModuleRef } from '@nestjs/core';
 import { getApp } from './app';
 
 export class NestWorld extends World {
   public readonly contextId = { id: 1 };
 
-  public async registerScenario() {
+  constructor(params: IWorldOptions) {
+    super(params);
+    this.registerScenario().catch((error) => {
+      console.error('Error registering scenario with NestJS');
+      console.error(error);
+      process.exit(1);
+    });
+  }
+
+  private async registerScenario() {
     const appModule = await getApp();
     const moduleRef = appModule.get(ModuleRef);
     moduleRef.registerRequestByContextId(this, this.contextId);
