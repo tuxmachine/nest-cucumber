e0945cf66649db5f0b44390fcc2a32960d69cd93
Rick Dutour Geerling <info@rickdg.nl>
2022-12-19 10:40:57 +0100

    chore: cleanup compiled files

--- tests/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_module_1 = require("./support/app.module");
-const lib_1 = require("../lib");
-(0, lib_1.bootstrap)(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
--- tests/support/sums/sum.steps.d.ts
+++
@@ -1,7 +0,0 @@
-import { NestWorld } from '../../../lib';
-export declare class SumSteps {
-    private numbers;
-    constructor(world: NestWorld);
-    givenASum(left: number, right: number): void;
-    thenReturn(expected: number): void;
-}
