1e7b8645e5200e204c37348e7ac89595d3fa900c
Rick Dutour Geerling <info@rickdg.nl>
2023-03-17 17:25:38 +0100

    fix: close app after all tests
    Required to exit tests if a database connection would otherwise keep the
    test alive

--- lib/setup.ts
+++ lib/setup.ts
@@ -1,10 +1,15 @@
+import * as Cucumber from '@cucumber/cucumber';
 import { setWorldConstructor } from '@cucumber/cucumber';
 import { Type } from '@nestjs/common';
 import 'reflect-metadata';
-import { startApp } from './app';
+import { getApp, startApp } from './app';
 import { NestWorld } from './world';
 
 export const bootstrap = (rootModule: Type) => {
   startApp(rootModule);
   setWorldConstructor(NestWorld);
+  Cucumber.AfterAll(async () => {
+    const app = await getApp();
+    await app.close();
+  });
 };
